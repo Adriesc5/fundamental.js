36f27bd97abd17329de3c6700d3c46822140c3f7
Francisco Escobedo <adriesc57@gmail.com>
2023-01-17 20:41:08 -0600

    Se hace ejercicio llamado arreglos dos donde trabajamos arreglos, para introducir nuevos datos para asi poder desplegarlos mandandolos llamar

---
+++ assets/Arreglos2.js
@@ -0,0 +1,45 @@
+/*let currencies = ['MXN','USD','EUR','CAD']
+console.log(currencies);
+
+//Conocer el tamano de un arreglo lenght
+
+let size = currencies.length;
+
+//Si queiro acceder a la ultima posicion del arreglo
+
+let last =currencies[currencies.length - 1]
+console.log(last);
+
+//Agregar un nuevo valor en el arreglo
+
+let new_currency = currencies.push('JPY');
+console.log({new_currency, currencies});
+
+//Vimos el punto lenght y el push, siempre lo arregla al final
+
+let add_init = currencies.unshift('NZD');
+//EL unshift lo agrega al principio 
+//Borra la ultima pocision pop
+let delete_last_pos =currencies.pop();
+
+//Borrar el valor de una pocision especifica
+
+let delete_esp_pos =currencies.splice(1,2);
+//Pocision indice de una moneda
+let currency_pos = currencies.indexOf('EUR')
+console.log({currency_pos,currencies})*/
+//Se crea ejercicio que nos pida tres numeros en pantalla
+
+let arr = [];
+let v1 = prompt('que valor deseas agregar primero?')
+arr.push(v1);
+
+let v2 = prompt('')
+arr.push(v2);
+
+let v3 = prompt('')
+arr.push(v3);
+let suma = parseInt(v1) + parseInt(v2) + parseInt(v3);
+document.write('<h1>el primer valor es:</h1>'+arr[0]);
+document.write('<h2>el ultimo valor es:</h2>'+arr[2]);
+document.write('<h3>la suma es:</h3>'+suma);
---
+++ assets/array.js
@@ -0,0 +1,11 @@
+//Datos en un arreglo
+
+let crypto = ['BTC','XRP','ETH','TRON']
+console.table({crypto});
+
+let arreglo_cosas = ['carro',true, 1+4,
+                        ['oro','plata','bronce']
+                    ]
+console.table(arreglo_cosas)
+console.log(arreglo_cosas[0])
+console.log(arreglo_cosas[3][0])
